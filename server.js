1ac172f30e2ef3fbd90c41be5e8780596e7a3adf
Dikesh Kumar <dikeshkumar9406@gmail.com>
2023-06-07 13:53:04 +0530

    server changed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
+import http from 'http';
 dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
@@ -46,11 +47,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(helmet());
-app.use(cors(
-  {
-    origin: "*",
-  }
-));
+app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 app.use(cookieParser());
@@ -86,10 +83,11 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
+const server=http.createServer(app);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () => {
+    server.listen(port, () => {
     
       console.log(`Server is listening on port ${port}...`);
     });
